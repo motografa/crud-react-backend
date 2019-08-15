4658eaa20b428df921118919e1fc4a4aa716d889
Daniel T. Santos <danieltex@gmail.com>
2019-08-15 15:36:02 -0300

    Fixed resources path

--- src/main/java/com/projeto/controller/ExampleController.java
+++ src/main/java/com/projeto/controller/ExampleController.java
@@ -1,10 +1,8 @@
 package com.projeto.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +16,13 @@ import com.projeto.repository.ExampleRepository;
 @RequestMapping("/example")
 public class ExampleController {
 	
-	@Autowired
+
 	private ExampleRepository exampleRepository;
-	
+
+	public ExampleController(ExampleRepository exampleRepository) {
+		this.exampleRepository = exampleRepository;
+	}
+
 	@RequestMapping("/")
 	public String getExampleById(){
 		return "Spring is on";
