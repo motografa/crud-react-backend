f75b391d91631a7985e21e15f583d5af55ba78fc
Daniel T. Santos <danieltex@gmail.com>
2019-08-15 15:31:45 -0300

    Using ExampleController as RestController

--- src/main/java/com/projeto/controller/ExampleController.java
+++ src/main/java/com/projeto/controller/ExampleController.java
@@ -8,13 +8,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.projeto.model.Example;
 import com.projeto.repository.ExampleRepository;
 
-@Controller
-@RequestMapping("example")
+@RestController
+@RequestMapping("/example")
 public class ExampleController {
 	
 	@Autowired
